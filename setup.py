56e00ef4501cbdc834e0da9112c7158a951ae319
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-01-08 11:11:18 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,21 @@
 
 from distutils.core import setup
 
-setup(name='chef-solo-cup',
-      version='0.0.1',
-      description='Chef-Solo wrapper',
-      author='Jose Diaz-Gonzalez',
-      author_email='email@josediazgonzalez.com',
-      url='https://github.com/josegonzalez/chef-solo-cup',
-      scripts=['bin/chef-solo-cup'],
-      install_requires=open('requirements.txt').readlines(),
-      long_description=open('README.rst').read()
-      )
+setup(
+    name='chef-solo-cup',
+    version='0.0.1',
+    author='Jose Diaz-Gonzalez',
+    author_email='email@josediazgonzalez.com',
+    packages=['chef_solo_cup'],
+    scripts=['bin/chef-solo-cup'],
+    url='https://github.com/josegonzalez/chef-solo-cup',
+    license=open('LICENSE.txt').read(),
+    classifiers=[
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    description='Chef-Solo wrapper',
+    long_description=open('README.rst').read(),
+    install_requires=open('requirements.txt').readlines(),
+)
