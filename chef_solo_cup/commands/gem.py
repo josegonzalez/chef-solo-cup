f9983acfa9e23e3a1de5f48252673dd5c43ed308
Jose Diaz-Gonzalez <myaccounts@savant.be>
2015-02-05 23:58:19 -0500

    Add utf-8 encoding to all python files

--- chef_solo_cup/commands/gem.py
+++ chef_solo_cup/commands/gem.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import with_statement
 
 from chef_solo_cup.helpers import sudo_dry
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from chef_solo_cup import __version__
 
